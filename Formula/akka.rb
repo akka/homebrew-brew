0e8f032126f1525a70727a1df5acc37e1ef490cb
GitHub <noreply@github.com>
2025-04-17 09:04:24 +0000

    feat(cli): update akka CLI to the latest stable version

--- Formula/akka.rb
+++ Formula/akka.rb
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 class Akka < Formula
-    version "3.0.15"
+    version "3.0.16"
     desc "Akka :: Command Line Interface"
     homepage "https://akka.io"
     license "https://downloads.lightbend.com/website/legal/lightbend-commercial-software-license-agreement.txt"
@@ -21,16 +21,16 @@ class Akka < Formula
     if OS.mac?
         if Hardware::CPU.intel?
             url "https://downloads.akka.io/#{version}/akka_darwin_amd64_#{version}.tar.gz"
-            sha256 "5edc8f2b075580989a6e1a27124f0bf830adb5f2256c4cf2e51e5da870f781da"
+            sha256 "9c00443ffdbd7e794ae96f4e851a35659052a6d941244e70e18935c00f6f8684"
         else
             url "https://downloads.akka.io/#{version}/akka_darwin_arm64_#{version}.tar.gz"
-            sha256 "f3102f5214a958a8a9354ac575611c374477ab2689095371ab9e0ed074a9dcd1"
+            sha256 "51ba60e878a01768b9c8ea70b79f98c416a83cb46677758fb8c79aeb0c140041"
         end
     end
 
     if OS.linux?
         url "https://downloads.akka.io/#{version}/akka_linux_amd64_#{version}.tar.gz"
-        sha256 "c9293ec817516b0e7fb13aee385f31579f92ccf7b155a85f06c25382db378992"
+        sha256 "19562518af566ca405ef52be28def60853e5971c101dc2ec5bb23f5ebac7c282"
     end
 
     def install
